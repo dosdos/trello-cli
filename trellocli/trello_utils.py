0330be175df6b235649c1923eba1246dcc42a45e
dos <dav.santucci@gmail.com>
2022-07-05 16:34:33 +0200

    Add some docs as reference for the Trello objects

--- trellocli/trello_utils.py
+++ trellocli/trello_utils.py
@@ -1,7 +1,12 @@
 class Board:
+    """
+    Trello Boards.
+    The Board ID and name are required fields.
+    Docs: https://developer.atlassian.com/cloud/trello/rest/api-group-boards/#api-group-boards
+    """
     def __init__(self, json_obj: dict) -> None:
-        self.id = json_obj['id']
-        self.name = json_obj['name']
+        self.id = json_obj['id']  # required
+        self.name = json_obj['name']  # required
         self.url = json_obj.get('url')
         self.description = json_obj.get('desc')
         self.closed = json_obj.get('closed')
@@ -12,10 +17,15 @@ class Board:
 
 
 class Column:
+    """
+    The columns in a Trello Board are called "Lists" in the API doc.
+    The parent Board, the Column ID and the Column name are required fields.
+    Docs: https://developer.atlassian.com/cloud/trello/rest/api-group-boards/#api-boards-id-lists-get
+    """
     def __init__(self, json_obj: dict) -> None:
-        self.id = json_obj['id']
-        self.board_id = json_obj['idBoard']
-        self.name = json_obj['name']
+        self.id = json_obj['id']  # required
+        self.board_id = json_obj['idBoard']  # required (from the parent Board)
+        self.name = json_obj['name']  # required
         self.pos = json_obj.get('pos')
 
     def __repr__(self) -> str:
@@ -23,10 +33,15 @@ class Column:
 
 
 class Card:
+    """
+    Trello Cards.
+    The parent Column, the Card ID and the Card name are required fields.
+    Docs: https://developer.atlassian.com/cloud/trello/rest/api-group-cards/#api-group-cards
+    """
     def __init__(self, json_obj: dict) -> None:
-        self.id = json_obj['id']
-        self.board_id = json_obj['idBoard']
-        self.column_id = json_obj['idList']
+        self.id = json_obj['id']  # required
+        self.board_id = json_obj['idBoard']  # required (from the parent Board)
+        self.column_id = json_obj['idList']  # required (from the parent List)
         self.name = json_obj.get('name')
         self.comment = ''
         self.comment_id = None
